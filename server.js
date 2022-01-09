3bb005b75fc684aaa7c11aaac3d0028aed1e7afa
max-narawut <narawutmax@gmail.com>
2022-01-09 15:12:14 +0700

    update

--- server.js
+++ server.js
@@ -1,16 +1,7 @@
 //Install express server
 const express = require('express');
-const path = require('path');
-
 const app = express();
-
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/rsproject-frontend'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/rsproject-frontend/index.html'));
-});
-
+app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
