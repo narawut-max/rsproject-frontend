51b6d39934c4d9e666ca5b3773fc42c56f1741bf
max-narawut <narawutmax@gmail.com>
2022-01-10 23:52:16 +0700

    update 23.52

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express= require('express');
 
-const path=require('path');
+// const path=require('path');
 
 const app=express();
 
-app.use(express.static(__dirname+'/dist/rsprojectfrontend'));
+app.use(express.static(__dirname+'/dist'));
 
-app.get('*', function(req, res){
-    res.sendFile(path.join(__dirname+'/dist/rsprojectfrontend/index.html'))
-})
+// app.get('*', function(req, res){
+//     res.sendFile(path.join(__dirname+'/dist/rsprojectfrontend/index.html'))
+// })
 
 app.listen(process.env.POST || 8080);
