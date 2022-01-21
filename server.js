8a0479b322a5a2a2108dbf6a1a64992b30d6fef9
narawut-max <80165968+narawut-max@users.noreply.github.com>
2022-01-22 01:38:16 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,48 +1,13 @@
-// const express= require('express');
+const express= require('express');
 
 // const path=require('path');
 
-// const app=express();
+const app=express();
 
-// app.use(express.static(__dirname+'/dist/jobthai'));
+app.use(express.static(__dirname+'/dist/jobthai'));
 
-// app.get('*', function(req, res){
-//     res.sendFile(path.join(__dirname+'/dist/rsprojectfrontend/index.html'))
-// })
+app.get('*', function(req, res){
+    res.sendFile(path.join(__dirname+'/dist/index.html'))
+})
 
-// app.listen(process.env.PORT || 8080);
-
-// ===================================================================================
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const path = require('path');
-const app = express();
-const port = process.env.PORT || 8080
-
-app.use(express.static(path.join(__dirname, '/dist/jobthai/')));
-
-app.use(bodyParser.json());
-
-app.use(cors());
-
-
-
- const forceSSL = function () {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-        ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }}app.use(forceSSL());
-
-app.get('/*', function (req, res) {
-   res.sendFile(path.join(__dirname + '/dist/jobthai/index.html'));
-     });
-
- app.listen(port, () => {
-  console.log('Server started on port '+port);
-  });
+app.listen(process.env.PORT || 8080);
