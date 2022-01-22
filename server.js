7350e8d250cea320410e88ef9309b6795083ac8c
max-narawut <narawutmax@gmail.com>
2022-01-23 00:12:01 +0700

    no message

--- server.js
+++ server.js
@@ -9,5 +9,8 @@ app.use(express.static(__dirname+'/dist/jobthai'));
 // app.get('*', function(req, res){
 //     res.sendFile(path.join(__dirname+'/dist/rsprojectfrontend/index.html'))
 // })
+app.use('*', (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'angular build file path'));
+});
 
 app.listen(process.env.PORT || 8080);
