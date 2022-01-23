0cb4b3e119724258a9f9eea748b19916e769d4e1
max-narawut <narawutmax@gmail.com>
2022-01-23 17:49:41 +0700

    no message

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname + '/dist/jobthai'));
 // })
 
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "index.html"));
 });
 
 app.listen(process.env.PORT || 8080);
